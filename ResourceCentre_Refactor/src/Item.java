b8addbb8938d3d960beb0b63dd99f83e58289238
iango <iango@10.174.115.85>
2022-07-27 12:06:42 +0800

    ian

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -1,43 +1,74 @@
-
 public class Item {
-	private String assetTag;
-	private String description;
-	private String dueDate;
-	private boolean isAvailable;
+  private String assetTag;
+  private String description;
+  private String dueDate;
+  private boolean isAvailable;
 
-	public Item(String assetTag, String description) {
-		this.assetTag = assetTag;
-		this.description = description;
-		this.dueDate = "";
-		this.isAvailable = true;
-	}
-	public String toString() {
-		
-		// Write your codes here
-		return null;
-	}
-	public String getAssetTag() {
-		return assetTag;
-	}
+  public Item(String assetTag, String description) {
+    this.assetTag = assetTag;
+    this.description = description;
+    this.dueDate = "";
+    this.isAvailable = true;
+  }
+  public String toString() {
+    // Write your codes here
+    String itemInfo = String.format("%-10s %-30s %-10s %-10s",
+        assetTag,
+        description, 
+        showAvailability(isAvailable),
+        dueDate);
+    return itemInfo;
+  }
+  public String showAvailability(boolean isAvailable) {
+    String avail;
 
-	public String getDescription() {
-		return description;
-	}
+    if (isAvailable == true) {
+      avail = "Yes";
+    } else {
+      avail = "No";
+    }
+    return avail;
+  }
+  public String getAssetTag() {
+    return assetTag;
+  }
 
-	public String getDueDate() {
-		return dueDate;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public void setDueDate(String dueDate) {
-		this.dueDate = dueDate;
-	}
+  public String getDueDate() {
+    return dueDate;
+  }
 
-	public boolean getIsAvailable() {
-		return isAvailable;
-	}
+  public void setDueDate(String dueDate) {
+    this.dueDate = dueDate;
+  }
 
-	public void setIsAvailable(boolean isAvailable) {
-		this.isAvailable = isAvailable;
-	}
+  public boolean getIsAvailable() {
+    return isAvailable;
+  }
+
+  public void setIsAvailable(boolean isAvailable) {
+    this.isAvailable = isAvailable;
+  }
 }
+public class Chromebook extends Item{
+  private String os;
 
+  public Chromebook(String assetTag, String description, String os) {
+    super(assetTag, description);
+    this.os = os;
+  }
+
+  public String getOs() {
+    return os;
+  }
+  
+  public String toString(){
+    // Write your codes here
+    String output = super.toString();
+    output = String.format("%-63s %-20s", output, os);
+    return output;
+  }
+}
