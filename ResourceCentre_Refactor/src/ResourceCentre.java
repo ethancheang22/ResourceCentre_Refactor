116d57692b6f5363e6392a6d048f564b8bced7ac
Ethan Cheang <Ethan Cheang@LAPTOP-ECDKB5FQ>
2022-07-27 09:49:42 +0800

    commit by ethan

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -5,7 +5,7 @@ public class ResourceCentre {
 	// Student 1 - Ian
 	// Student 2 - Maegan
 	// Student 3 - Ethan
-	// Student 4 - Alvin 
+	// Student 4 - Alvin smelly
 	
 
 	private static final int OPTION_RETURN = 4;
